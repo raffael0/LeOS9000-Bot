7baa045c7f54703d0ecd68af043bcff3eea35ffc
nichtdasleo <40544875+nichtdasleo@users.noreply.github.com>
2018-07-23 21:03:31 +0200

    Update ScrambleCMD.java

--- src/main/java/Commands/Cubing/ScrambleCMD.java
+++ src/main/java/Commands/Cubing/ScrambleCMD.java
@@ -31,7 +31,7 @@ public class ScrambleCMD extends Command {
                     .setParameter("co", "15")
                     .setParameter("view", "trans")
                     .setParameter("cc","black")
-                    .setParameter("fo", "50")
+                    .setParameter("fo", "70")
                     .setParameter("alg", event.getArgs().replace('`', '\'').replaceAll("\\s+",""))
                     .build();
         } catch (URISyntaxException e) {
