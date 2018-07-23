1fc8ae7b1ba53f45d5cd5b29d753d15cec3ca987
nichtdasleo <40544875+nichtdasleo@users.noreply.github.com>
2018-07-23 20:46:20 +0200

    added scramble, web and image command and made a few minor changes

---
+++ src/main/java/Commands/Cubing/ScrambleCMD.java
@@ -0,0 +1,44 @@
+package Commands.Cubing;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import net.dv8tion.jda.core.EmbedBuilder;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class ScrambleCMD extends Command {
+    public ScrambleCMD(){
+        this.name="scramble";
+        this.arguments="<scramble>";
+        this.help="returns an image of your scramble";
+    }
+
+    @Override
+    protected void execute(CommandEvent event) {
+        EmbedBuilder eb = new EmbedBuilder();
+
+        URI uri = null;
+        try {
+            uri = new URIBuilder()
+                    .setScheme("http")
+                    .setHost("roudai.net")
+                    .setPath("/visualcube/visualcube.php")
+                    .setParameter("fmt", "png")
+                    .setParameter("size", "500")
+                    .setParameter("co", "15")
+                    .setParameter("view", "trans")
+                    .setParameter("cc","black")
+                    .setParameter("alg", event.getArgs().replace('`', '\'').replaceAll("\\s+",""))
+                    .build();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        HttpGet httpget = new HttpGet(uri);
+        eb.setImage(httpget.getURI().toString());
+        eb.setTitle(event.getArgs());
+        event.reply(eb.build());
+    }
+}
--- src/main/java/Core/Main.java
+++ src/main/java/Core/Main.java
@@ -1,10 +1,10 @@
 
 package Core;
 
-import Commands.Administration.GameCommand;
-import Commands.Administration.PrefixCommand;
-import Commands.Administration.RebootCommand;
-import Commands.Administration.UpdateCommand;
+import Commands.Administration.*;
+import Commands.Cubing.ScrambleCMD;
+import Commands.Fun.ImageCMD;
+import Commands.Fun.WebCMD;
 import Commands.Information.GithubCMD;
 import Commands.Information.InviteCMD;
 import Commands.UserInteraction.FeedbackCommand;
@@ -14,6 +14,7 @@ import Listeners.ReadyListener;
 import Util.BotUtil;
 import com.jagrosh.jdautilities.command.CommandClient;
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
@@ -21,20 +22,26 @@ import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.entities.Guild;
 
 import javax.security.auth.login.LoginException;
+import java.awt.*;
 
 public class Main {
     static String token = BotUtil.getToken();
     static JDA jda = null;
+    static EventWaiter waiter = null;
 
     public static void main(String[] args){
         JDABuilder builder = new JDABuilder(AccountType.BOT).setToken(token).setAutoReconnect(true);
 
         CommandClientBuilder cbuilder = new CommandClientBuilder().setPrefix(BotUtil.getPrefix()).setOwnerId(BotUtil.getOwner());
 
-        cbuilder.addCommands(new GameCommand(), new PrefixCommand(), new RebootCommand(), new UpdateCommand(), new GlobalChatCMD(), new FeedbackCommand(), new GithubCMD(), new InviteCMD());
+        cbuilder.addCommands(new GameCommand(), new PrefixCommand(), new RebootCommand(), new UpdateCommand(), new GlobalChatCMD(), new FeedbackCommand(), new GithubCMD(), new InviteCMD(),
+                new WatchingCommand(), new WebCMD(), new ImageCMD(), new ScrambleCMD());
 
         CommandClient client = cbuilder.build();
 
+        waiter = new EventWaiter();
+
+        builder.addEventListener(waiter);
         builder.addEventListener(new ReadyListener());
         builder.addEventListener(new GlobalChatListener());
         builder.addEventListener(client);
@@ -56,4 +63,6 @@ public class Main {
     public static JDA getJDA(){
         return jda;
     }
+
+    public static EventWaiter getWaiter(){ return waiter; };
 }
