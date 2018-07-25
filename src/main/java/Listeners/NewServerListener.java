346733403038f30ed04128cf0d3dcbf76c75f7fa
nichtdasleo <40544875+nichtdasleo@users.noreply.github.com>
2018-07-25 22:14:51 +0200

    a

--- src/main/java/Listeners/NewServerListener.java
+++ src/main/java/Listeners/NewServerListener.java
@@ -3,13 +3,19 @@ package Listeners;
 import Util.BotUtil;
 import Util.CubingCMDUtil;
 import net.dv8tion.jda.core.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.core.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class NewServerListener extends ListenerAdapter {
     @Override
     public void onGuildJoin(GuildJoinEvent event){
         CubingCMDUtil.setFalse(event.getGuild().getId());
+        event.getJDA().getTextChannelById(BotUtil.getStatus()).sendMessage("just joined server ```" + event.getGuild().getName() + "``` (" + event.getGuild().getId() + "), which has " + event.getGuild().getMembers().size() + " members").queue();
+    }
 
-        event.getJDA().getTextChannelById(BotUtil.getStatus()).sendMessage("Just joined server \"" + event.getGuild().getName() + "\" (" + event.getGuild().getId() + "), which has " + event.getGuild().getMembers().size() + " members").queue();
+    @Override
+    public void onGuildLeave(GuildLeaveEvent event){
+        CubingCMDUtil.setFalse(event.getGuild().getId());
+        event.getJDA().getTextChannelById(BotUtil.getStatus()).sendMessage("just left server ```" + event.getGuild().getName() + "``` (" + event.getGuild().getId() + "), which has" + event.getGuild().getMembers().size() + " members").queue();
     }
 }
