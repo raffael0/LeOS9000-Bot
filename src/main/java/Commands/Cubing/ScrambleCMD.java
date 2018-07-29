60355566d21aed9f1dd789bfbe0568b67897db9b
nichtdasleo <40544875+nichtdasleo@users.noreply.github.com>
2018-07-29 20:22:23 +0200

    Update ScrambleCMD.java

--- src/main/java/Commands/Cubing/ScrambleCMD.java
+++ src/main/java/Commands/Cubing/ScrambleCMD.java
@@ -18,7 +18,7 @@ public class ScrambleCMD extends Command {
 
         eb.setTitle("Scramble Command");
         eb.setThumbnail(Main.getJDA().getSelfUser().getAvatarUrl());
-        eb.setDescription("Use one of the following commands to an image of a given scramble. Put your scramble after the command, like this: \n\n.sc3 R U R' U'\n\nDifferent Commands:");
+        eb.setDescription("Use one of the following commands to an image of a given scramble. Put your scramble after the command, like this: \n\n-sc3 R U R' U'\n\nDifferent Commands:");
         eb.addField("WCA", "sc2 - 2x2\nsc3 - 3x3\nsc4 - 4x4\nsc5 - 5x5\nsc6 - 6x6\nsc7 - 7x7", true);
         eb.addField("Subsets", "cross - Cross\nf2l - F2L\noll - OLL\nf2b - F2B\ncmll - CMLL", true);
 
