849490aa578b7d072aadfa734b3df3a3f1b215e2
nichtdasleo <40544875+nichtdasleo@users.noreply.github.com>
2018-08-01 12:49:44 +0200

    a

--- src/main/java/Listeners/GlobalChatListener.java
+++ src/main/java/Listeners/GlobalChatListener.java
@@ -36,6 +36,29 @@ public class GlobalChatListener extends ListenerAdapter {
                     ).queue();
                 }
             }
+
+            if (!event.getMessage().getAttachments().isEmpty()) {
+                String content;
+
+                if(!event.getMessage().getContentStripped().isEmpty()) {
+                    content = "```" + event.getMessage().getContentStripped() + "```\n";
+                } else{
+                    content = "";
+                }
+
+                if (!event.getMessage().getAttachments().isEmpty()) {
+                    event.getJDA().getGuildById(BotUtil.getServer()).getTextChannelsByName("global-log", true).get(0).sendMessage(
+                            event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator() + " in \"" +
+                                    GlobalChatUtil.getServerName(event.getGuild().getId()) + "\" (" + event.getGuild().getId() + "): " + content + event.getMessage().getAttachments().get(0).getUrl()
+                    ).queue();
+                } else {
+                    event.getJDA().getGuildById(BotUtil.getServer()).getTextChannelsByName("global-log", true).get(0).sendMessage(
+                            event.getAuthor().getName() + "#" + event.getAuthor().getDiscriminator() + " in \"" +
+                                    GlobalChatUtil.getServerName(event.getGuild().getId()) + "\" (" + event.getGuild().getId() + "): \n" + "```"
+                                    + event.getMessage().getContentStripped() + "```"
+                    ).queue();
+                }
+            }
         }
     }
 }
