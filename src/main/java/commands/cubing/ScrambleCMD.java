6ef7487f97d2e379525d201ccb2273c3ef9a0971
Unknown <nunner.leo@gmail.com>
2018-08-11 14:27:53 +0200

    changed summary of scramble command
    removed subsets for now

--- src/main/java/commands/cubing/ScrambleCMD.java
+++ src/main/java/commands/cubing/ScrambleCMD.java
@@ -21,7 +21,7 @@ public class ScrambleCMD extends Command {
         eb.setThumbnail(Main.getJDA().getSelfUser().getAvatarUrl());
         eb.setDescription("Use one of the following commands to an image of a given scramble. Put your scramble after the command, like this: \n\n-sc3 R U R' U'\n\nDifferent bot.commands:");
         eb.addField("wca", "sc2 - 2x2\nsc3 - 3x3", true);
-        eb.addField("subsets", "cross - Cross\nf2l - F2L\noll - OLL\nf2b - F2B\ncmll - CMLL", true);
+        eb.addField("Important, ","Cubes bigger than 3x3 are currently disabled. They will be back soon", false);
         eb.setFooter("images are generated using JavaCube!", event.getSelfUser().getAvatarUrl());
 
         event.reply(eb.build());
